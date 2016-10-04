35089f0d6e50eaa4438ea36a33aa102a12c5be33
LICHAO <515359166@qq.com>
2016-10-04 22:54:40 +0800

    N

---
+++ Oct_01.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <iostream>
+int main()
+{
+	/*printf("please input a number:\n");
+	long long n;
+	scanf("%lld",&n);
+	printf("%lld",n);
+	printf("\n");
+	int s;
+		while(n!=0)
+	{	
+		long long l=n%10;
+		n=n/10;
+		s+=l;
+	}
+	printf("%d",s);
+	printf("\n");
+//	long	int f= 123456;
+//	printf("%d,%ld\n",f,f);
+	char c=65;
+	printf("%c",c);
+	printf("\a");
+	int a;
+	int b;
+	scanf("%d%d",&a,&b);
+	printf("the max number in a and b is:%d",a>=b?a:b);
+	int f =89;
+	printf("%d","%d",f,f);
+
+
+	int a;
+	scanf("%d",&a);
+	if(a>70)
+		if(a>80)
+			if(a>90)
+				printf("A");
+			else
+				printf("error  grades!!");
+		else
+			printf("B");
+	else
+		printf("C");
+
+	printf("\nplease input an >9999 number\n");
+	int s;	
+	while(1)
+	{
+		printf("please input an >9999 number\n");
+		
+		scanf("%d",&s);
+		if (s>9999)
+			break;
+		else
+			continue;
+	}
+	printf("%d\n",s);
+	int l=0;
+	while(s!=0)
+	{
+	//	int l;
+		l+=(s%10);
+		printf("%d\n",l);
+		s /=10;
+	}
+	printf("%d\n",l);
+	int i=2,sum=0;
+	while(1)
+	{	if (sum>20)
+		break;
+		sum+=i;i+=2;
+	}
+	printf("%d\n",sum);
+
+	return 0;*/
+//计算n的阶乘
+	/*int n,i,s=1;
+	scanf("%d",&n);
+	for (i=1;i<=n;i++)
+	{	s*=i;}
+	printf("%d\n",s);
+			return 0;*/
+//Fabniqi数列的弟n项
+	int n,i,s=1;
+	scanf("%d",&n);
+	for(i=0;i<=n;i++)
+	{	
+		
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
