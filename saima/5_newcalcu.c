d8eb3c3f40bb05e68015046905d6400ba50e5f88
LICHAO <515359166@qq.com>
2016-10-16 00:12:49 +0800

    5

---
+++ saima/5_newcalcu.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void print(int i)
+{
+	printf("   \n | \n    \n | \n   \n");
+}
+void print(int i)
+{
+	printf(" - \n  |\n - \n|  \n - \n");
+}
+int main()
+{
+	print2();
+	return 0;
+}
